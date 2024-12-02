e27c0602ef89ecae80a8b67115a67ae43313c616
Ciaran Treanor <ciaran.treanor@gmail.com>
2024-12-02 06:43:32 +0000

    Day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,21 +1,40 @@
+import kotlin.math.absoluteValue
+
 fun main() {
+
+    fun extractReports(input: List<String>) = input.map { line ->
+        line.split(" ").map { it.toInt() }
+    }
+
+    fun isReportSafe(report: List<Int>): Boolean =
+        report.zipWithNext { a, b -> b - a }
+            .let { differences ->
+                differences.all { it in 1..3 } || differences.all { it in -3..-1 }
+            }
+
+    fun isReportSafeWithDampener(report: List<Int>): Boolean =
+        isReportSafe(report) || report.indices.any { index ->
+            isReportSafe(report.filterIndexed { i, _ -> i != index })
+        }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val reports = extractReports(input)
+
+        return reports.count(::isReportSafe)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val reports = extractReports(input)
+
+        return reports.count(::isReportSafeWithDampener)
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    // Tests
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
 }

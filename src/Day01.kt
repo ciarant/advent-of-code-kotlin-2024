ffbdd657a6b351dd24dc2af7ba12b1af419462cf
Ciaran Treanor <ciaran.treanor@gmail.com>
2024-12-11 15:50:20 +0000

    Day01: Make things a little more succinct

--- src/Day01.kt
+++ src/Day01.kt
@@ -6,18 +6,14 @@ import kotlin.math.absoluteValue
 fun main() {
 
     fun splitLists(input: List<String>): Pair<List<Int>, List<Int>> =
-        input.mapNotNull { line ->
-            line.split(' ').filter { it.isNotEmpty() }.takeIf { it.size == 2 }
-                ?.let { it[0].toInt() to it[1].toInt() }
+        input.map { line ->
+            line.split("   ").let { (left, right) -> left.toInt() to right.toInt() }
         }.unzip()
 
     fun part1(input: List<String>): Int {
         val (leftList, rightList) = splitLists(input)
 
-        val sortedLeftList = leftList.sorted()
-        val sortedRightList = rightList.sorted()
-
-        val totalDistance = sortedLeftList.zip(sortedRightList)
+        val totalDistance = leftList.sorted().zip(rightList.sorted())
             .sumOf { (left, right) -> kotlin.math.abs(left - right) }
 
         return totalDistance;
@@ -26,11 +22,9 @@ fun main() {
     fun part2(input: List<String>): Int {
         val (leftList, rightList) = splitLists(input)
 
-        // Count occurrences of each number in the right list
-        val rightCounts = rightList.groupingBy { it }.eachCount()
-
-        // Calculate the similarity score
-        return leftList.sumOf { it * (rightCounts[it] ?: 0) }
+        return leftList.sumOf { left ->
+            left * rightList.count { it == left }
+        }
     }
 
     // Tests
